ddc14a287713c50fdcac5158b41de648e34e9f7f
Debora Benitez <deborabenitez@live.com.ar>
2023-03-29 00:24:29 -0300

    state redux

---
+++ src/constants/colors.js
@@ -0,0 +1,5 @@
+export const COLORS = {
+    primary: '#FADDAA',
+    accent: '#f54242',
+    black: '#000',
+}
---
+++ src/data/categories.js
@@ -0,0 +1,22 @@
+export const CATEGORIES = [
+    {
+        id: '1',
+        title: 'Harinas Puras',
+        color: '#F0ECD8'
+    },
+    {
+        id: '2',
+        title: 'Harinas Integrales',
+        color: '#CFC6B0'
+    },
+    {
+        id: '3',
+        title: 'Panes Saborizados',
+        color: '#f5a442'
+    },
+    {
+        id: '4',
+        title: 'Otros Productos de Panadería',
+        color: '#f5d142'
+    }
+]
